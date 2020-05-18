4125561c68c1a3cfd1dd10e2de6189b6a3deaa42
pedjo95 <tec.ped95@gmail.com>
2020-05-18 20:19:42 -0300

    Using localStorage to save the locations

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 // init weather class
-const weather = new Weather("London", "uk");
+const storage = new Storage();
+const weatherLocation = storage.getLocationData();
+const weather = new Weather(weatherLocation.city, weatherLocation.country);
 const ui = new UI();
 
-// weather.changeLocation("Miami", "us");
-
 document.addEventListener("DOMContentLoaded", getWeather);
+
+document.getElementById("w-change-btn").addEventListener("click", (e) => {
+  const city = document.getElementById("city").value;
+
+  const country = document.getElementById("country").value;
+
+  weather.changeLocation(city, country);
+
+  storage.setLocationData(city, country);
+
+  getWeather();
+
+  $("#locModal").modal("hide");
+});
 
 function getWeather() {
   weather
     .getWeather()
     .then((results) => {
-      console.log(results);
       ui.paint(results);
     })
     .catch((err) => console.log(err));
